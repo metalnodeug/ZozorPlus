a8d9701fae0e7ece7e60e8f333475430d87b29ec
metalnodeug <metalnodeug@gmail.com>
2019-02-04 12:48:43 +0100

    Add multiple correction

--- Zozor+/Model/Model.swift
+++ Zozor+/Model/Model.swift
@@ -7,3 +7,82 @@
 //
 
 import Foundation
+
+class Calculate {
+    
+    // MARK: - Properties
+    var index = 0
+    var stringNumbers: [String] = [String()]
+    var operators: [String] = ["+"]
+    
+    
+    // MARK: - Methods
+    //check if expression is correct
+    var isExpressionCorrect: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                return false
+            }
+        }
+        return true
+    }
+    
+    //Check if expression are correct
+    var canAddOperator: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func addNewOperator(_ newOperator: String) throws {
+        guard canAddOperator else {
+            throw ErrorMessage.cantAddOperator
+        }
+        operators.append(newOperator)
+        stringNumbers.append("")
+    }
+    
+    //Clear TextField
+    func clear() {
+        stringNumbers = [String()]
+        operators = ["+"]
+        index = 0
+    }
+    
+    func addNewNumber(_ newNumber: Int) {
+        if let stringNumber = stringNumbers.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumbers[stringNumbers.count-1] = stringNumberMutable
+        }
+    }
+    
+    func calculateTotal() {
+        if !isExpressionCorrect {
+            return
+        }
+        
+        var total = 0
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            if let number = Int(stringNumber) {
+                switch operators[i] {
+                case "+": total += number
+                case "-": total -= number
+                case "/": total = total / number
+                case "x": total = total * number
+                default: break
+                }
+                
+            }
+        }
+        
+        // textView.text = textView.text + "=\(total)"
+        
+        clear()
+    }
+    
+    
+}
